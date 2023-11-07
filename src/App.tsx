9b9d3d5446025a43c34ca98283396669bfc350e4
shusaku009 <shusaku009@gmail.com>
2023-11-08 08:59:24 +0900

    add: ユーザーカードを作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './App.css';
+import { UserCard } from './components/UserCard';
+
+const user = {
+  id: 1,
+  name: "しゅう",
+  email: "sample@email.com",
+  address: "address"
+}
 
 function App() {
   return (
     <div className="App">
+      <UserCard user={user}/>
     </div>
   );
 }
